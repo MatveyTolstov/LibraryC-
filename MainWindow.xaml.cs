65f7badb5cb49f981ebcaf9c2cab1be51239ded8
MatveyTolstov <144270275+MatveyTolstov@users.noreply.github.com>
2024-05-31 23:22:32 +0300

    LibraryC#

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+using SerorDel;
+
+
+namespace Library
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+                InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SerorDel.Class1.Serializee(TextBx.Text, "mama");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            openFileDialog.ShowDialog();
+            string file = openFileDialog.FileName;
+            if (file != null)
+            {
+                var result = SerorDel.Class1.DeserializeFromFile<object>(file);
+                TextBx.Text = result.ToString();
+            }
+
+            
+        }
+    }
+}
